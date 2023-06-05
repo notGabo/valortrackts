011403fc84a9577b04ba7875f9d04339eecfd3ae
Gabriel Soto <gsoto@analytics.cl>
2023-06-04 22:49:03 -0400

    fix eliminar carta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { Fragment, useState} from "react";
 import { Accordion, AccordionHeader, AccordionBody } from "@material-tailwind/react";
 import Carrusel from "./components/Carrusel";
 import Image from "next/image";
-import Carta from "./components/Carta";
 import HomeBody from "./components/homeBody";
 
 const home = () => {
